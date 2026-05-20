f0de67ab328b3126b56c82dba0b2a4ad2eb2512d
mikhaildadaev <mikhaildadaev@gmail.com>
2026-05-20 16:46:28 +0300

    Обновление синтаксиса

--- uast.go
+++ uast.go
@@ -376,7 +376,7 @@ var listFunctionServices = []functionService{
 }
 var listManagementServices = []managementService{uastManagementDatabase, uastManagementCurrentUser, uastManagementSessionUser, uastManagementSystemUser, uastManagementUser, uastManagementVersion, uastManagementBenchmark, uastManagementDelay, uastManagementSleep, uastManagementTimeout, uastManagementWaitFor, uastManagementAlter, uastManagementCreate, uastManagementDrop, uastManagementDelete, uastManagementInsert, uastManagementSelect, uastManagementUpdate, uastManagementTruncate, uastManagementFrom, uastManagementInto, uastManagementSet, uastManagementTo, uastManagementGroupBy, uastManagementHaving, uastManagementJoin, uastManagementLimit, uastManagementOffset, uastManagementOrderBy, uastManagementReturning, uastManagementUsing, uastManagementValues, uastManagementWhere, uastManagementWith}
 var listModifierServices = []modifierService{uastModifierAnd, uastModifierAs, uastModifierBetween, uastModifierDistinct, uastModifierElse, uastModifierEnd, uastModifierInterval, uastModifierOn, uastModifierOver, uastModifierRecursive, uastModifierSeparator, uastModifierThen, uastModifierWhen}
-var listSupportDialects = []*SupportDialect{DialectMySQL, DialectPostgreSQL, DialectSQLite}
+var listSupportDialects = []*SupportDialect{DialectMariaDB, DialectMySQL, DialectPostgreSQL, DialectSQLite}
 var listSymbolSafeAlias = [256]bool{
 	'+': true, '-': true, '_': true,
 	'0': true, '1': true, '2': true, '3': true, '4': true, '5': true, '6': true, '7': true, '8': true, '9': true,

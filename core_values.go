83cbaef44b65340b71defcbfb3fa922100265605
mikhaildadaev <mikhaildadaev@gmail.com>
2026-05-22 14:27:48 +0300

    Обновление структуры

--- core_values.go
+++ core_values.go
@@ -15,12 +15,48 @@ type clausePair struct {
 	column markExpressable
 	value  ExpressionBase
 }
+type clauseUpsert struct {
+	pairs   []*clausePair
+	service managementService
+}
 type clauseValues struct {
 	pairs  []*clausePair
 	upsert *clauseUpsert
 }
 
 // Приватные методы
+func (clauseUpsert *clauseUpsert) render(baseRenderer *baseRenderer) error {
+	baseRenderer.renderOperator(uastCompositeSingleSpace)
+	baseRenderer.renderService(clauseUpsert.service)
+	for i, pair := range clauseUpsert.pairs {
+		if i > 0 {
+			baseRenderer.renderOperator(uastCompositeCommaSpace)
+		}
+		baseRenderer.renderOperator(uastCompositeSingleSpace)
+		if err := pair.column.render(baseRenderer); err != nil {
+			return err
+		}
+		baseRenderer.renderOperator(uastCompositeSpaceEqualSpace)
+		if err := pair.value.render(baseRenderer); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+func (clauseUpsert *clauseUpsert) validate(baseValidator *baseValidator) error {
+	if clauseUpsert.pairs == nil {
+		return ErrInvalidStatementSet
+	}
+	for _, pair := range clauseUpsert.pairs {
+		if err := pair.column.validate(baseValidator); err != nil {
+			return err
+		}
+		if err := pair.value.validate(baseValidator); err != nil {
+			return err
+		}
+	}
+	return nil
+}
 func (clauseValues *clauseValues) render(baseRenderer *baseRenderer) error {
 	for _, pair := range clauseValues.pairs {
 		if err := pair.value.render(baseRenderer); err != nil {

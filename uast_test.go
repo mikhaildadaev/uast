588cc0f481cea1c63f374d58f6c17ebdb7527638
mikhaildadaev <mikhaildadaev@gmail.com>
2026-06-17 14:58:43 +0300

    Обновление тестов

--- uast_test.go
+++ uast_test.go
@@ -1945,15 +1945,15 @@ func Test_SQL_Alter(t *testing.T) {
 			sqlAlterQuery, sqlAlterArguments, err := sql.Build(stmtAlter)
 			switch supportDialect {
 			case DialectMariaDB:
-				assertContains(t, sqlAlterQuery, "ALTER TABLE `users`", "ALTER TABLE")
+				assertContains(t, sqlAlterQuery, "ALTER TABLE `users` ADD COLUMN `string` VARCHAR, ADD COLUMN `date` DATE, ADD CONSTRAINT `ck_orders_number` CHECK(`o`.`number` > ?), ADD CONSTRAINT `pk_orders_id` PRIMARY KEY(`id`), ADD CONSTRAINT `un_orders_name` UNIQUE(`string`), DROP COLUMN `id`, DROP COLUMN `name`, DROP CONSTRAINT `ck_users_number`, DROP CONSTRAINT `fk_users_orders`, DROP CONSTRAINT `pk_users_id`, DROP CONSTRAINT `un_users_name`", "ALTER TABLE")
 			case DialectMsSQL:
-				assertContains(t, sqlAlterQuery, "ALTER TABLE [users]", "ALTER TABLE")
+				assertContains(t, sqlAlterQuery, "ALTER TABLE [users] ADD COLUMN [string] NVARCHAR, ADD COLUMN [date] DATE, ADD CONSTRAINT [ck_orders_number] CHECK([o].[number] > @p1), ADD CONSTRAINT [pk_orders_id] PRIMARY KEY([id]), ADD CONSTRAINT [un_orders_name] UNIQUE([string]), DROP COLUMN [id], DROP COLUMN [name], DROP CONSTRAINT [ck_users_number], DROP CONSTRAINT [fk_users_orders], DROP CONSTRAINT [pk_users_id], DROP CONSTRAINT [un_users_name]", "ALTER TABLE")
 			case DialectMySQL:
-				assertContains(t, sqlAlterQuery, "ALTER TABLE `users`", "ALTER TABLE")
+				assertContains(t, sqlAlterQuery, "ALTER TABLE `users` ADD COLUMN `string` VARCHAR, ADD COLUMN `date` DATE, ADD CONSTRAINT `ck_orders_number` CHECK(`o`.`number` > ?), ADD CONSTRAINT `pk_orders_id` PRIMARY KEY(`id`), ADD CONSTRAINT `un_orders_name` UNIQUE(`string`), DROP COLUMN `id`, DROP COLUMN `name`, DROP CONSTRAINT `ck_users_number`, DROP CONSTRAINT `fk_users_orders`, DROP CONSTRAINT `pk_users_id`, DROP CONSTRAINT `un_users_name`", "ALTER TABLE")
 			case DialectPostgreSQL:
-				assertContains(t, sqlAlterQuery, `ALTER TABLE "users"`, "ALTER TABLE")
+				assertContains(t, sqlAlterQuery, `ALTER TABLE "users" ADD COLUMN "string" VARCHAR, ADD COLUMN "date" DATE, ADD CONSTRAINT "ck_orders_number" CHECK("o"."number" > $1), ADD CONSTRAINT "pk_orders_id" PRIMARY KEY("id"), ADD CONSTRAINT "un_orders_name" UNIQUE("string"), DROP COLUMN "id", DROP COLUMN "name", DROP CONSTRAINT "ck_users_number", DROP CONSTRAINT "fk_users_orders", DROP CONSTRAINT "pk_users_id", DROP CONSTRAINT "un_users_name"`, "ALTER TABLE")
 			case DialectSQLite:
-				assertContains(t, sqlAlterQuery, `ALTER TABLE "users"`, "ALTER TABLE")
+				assertContains(t, sqlAlterQuery, `ALTER TABLE "users" ADD COLUMN "string" TEXT, ADD COLUMN "date" TEXT, ADD CONSTRAINT "ck_orders_number" CHECK("o"."number" > ?), ADD CONSTRAINT "pk_orders_id" PRIMARY KEY("id"), ADD CONSTRAINT "un_orders_name" UNIQUE("string"), DROP COLUMN "id", DROP COLUMN "name", DROP CONSTRAINT "ck_users_number", DROP CONSTRAINT "fk_users_orders", DROP CONSTRAINT "pk_users_id", DROP CONSTRAINT "un_users_name"`, "ALTER TABLE")
 			}
 			t.Logf("\nerr: [%s] \nsdn: %s \nsqa: [%s] \nsql: [%s]", err, sqlAlterArguments, supportDialect.name, sqlAlterQuery)
 		})

f0e083f90fb718d359feb46005c92fdd9af0534d
mikhaildadaev <mikhaildadaev@gmail.com>
2026-07-01 12:55:25 +0300

    Обновление тестов

--- uast_test.go
+++ uast_test.go
@@ -2022,15 +2022,15 @@ func Test_SQL_Comment(t *testing.T) {
 			sqlCommentQuery, sqlCommentArguments, err := sql.Build(stmtComment)
 			switch supportDialect {
 			case DialectMariaDB:
-				//assertContains(t, sqlCommentQuery, "COMMENT ON TABLE `users` IS 'text'", "COMMENT")
+				assertContains(t, sqlCommentQuery, "COMMENT ON TABLE `users` IS 'text'", "COMMENT")
 			case DialectMsSQL:
 				// Not supported - COMMENT [ON TABLE]
 			case DialectMySQL:
-				//assertContains(t, sqlCommentQuery, "COMMENT ON TABLE `users` IS 'text'", "COMMENT")
+				assertContains(t, sqlCommentQuery, "COMMENT ON TABLE `users` IS 'text'", "COMMENT")
 			case DialectPostgreSQL:
-				//assertContains(t, sqlCommentQuery, `COMMENT ON TABLE "users" IS 'text'`, "COMMENT")
+				assertContains(t, sqlCommentQuery, `COMMENT ON TABLE "users" IS 'text'`, "COMMENT")
 			case DialectSQLite:
-				//assertContains(t, sqlCommentQuery, `COMMENT ON TABLE "users" IS 'text'`, "COMMENT")
+				assertContains(t, sqlCommentQuery, `COMMENT ON TABLE "users" IS 'text'`, "COMMENT")
 			}
 			t.Logf("\nerr: [%s] \nsdn: %s \nsqa: [%s] \nsql: [%s]", err, sqlCommentArguments, supportDialect.name, sqlCommentQuery)
 		})

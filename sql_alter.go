ad395cdebc42b60ef33c978270be66ee24cc5771
mikhaildadaev <mikhaildadaev@gmail.com>
2026-07-04 13:07:35 +0300

    Обновление стейтмента

--- sql_alter.go
+++ sql_alter.go
@@ -32,6 +32,20 @@ func (stmt *stmtAlter) DropConstraints(constraints ...ConstraintBase) *stmtAlter
 	stmt.dropConstraints = append(stmt.dropConstraints, constraints...)
 	return stmt
 }
+func (stmt *stmtAlter) RenameColumn(column SourceBase, name string) *stmtAlter {
+	stmt.renameColumn = &columnRename{
+		column: column,
+		name:   name,
+	}
+	return stmt
+}
+func (stmt *stmtAlter) RenameConstraint(constraint ConstraintBase, name string) *stmtAlter {
+	stmt.renameConstraint = &constraintRename{
+		constraint: constraint,
+		name:       name,
+	}
+	return stmt
+}
 func (stmt *stmtAlter) RenameTo(name string) *stmtAlter {
 	stmt.renameTo = name
 	return stmt
@@ -39,21 +53,65 @@ func (stmt *stmtAlter) RenameTo(name string) *stmtAlter {
 
 // Приватные структуры
 type stmtAlter struct {
-	command         managementService
-	addColumns      []markSourceable
-	addConstraints  []ConstraintBase
-	dropColumns     []markSourceable
-	dropConstraints []ConstraintBase
-	entity          SourceBase
-	ifExists        bool
-	ifNotExists     bool
-	on              SourceBase
-	renameTo        string
+	command          managementService
+	addColumns       []markSourceable
+	addConstraints   []ConstraintBase
+	dropColumns      []markSourceable
+	dropConstraints  []ConstraintBase
+	entity           SourceBase
+	ifExists         bool
+	ifNotExists      bool
+	on               SourceBase
+	renameColumn     *columnRename
+	renameConstraint *constraintRename
+	renameTo         string
 }
 
 // Приватные методы
 func (stmt *stmtAlter) clone() statement {
 	copy := *stmt
+	if len(stmt.addColumns) > 0 {
+		copy.addColumns = make([]markSourceable, len(stmt.addColumns))
+		for i, col := range stmt.addColumns {
+			copy.addColumns[i] = col.clone().(markSourceable)
+		}
+	}
+	if len(stmt.addConstraints) > 0 {
+		copy.addConstraints = make([]ConstraintBase, len(stmt.addConstraints))
+		for i, constraint := range stmt.addConstraints {
+			copy.addConstraints[i] = constraint.clone()
+		}
+	}
+	if len(stmt.dropColumns) > 0 {
+		copy.dropColumns = make([]markSourceable, len(stmt.dropColumns))
+		for i, col := range stmt.dropColumns {
+			copy.dropColumns[i] = col.clone().(markSourceable)
+		}
+	}
+	if len(stmt.dropConstraints) > 0 {
+		copy.dropConstraints = make([]ConstraintBase, len(stmt.dropConstraints))
+		for i, constraint := range stmt.dropConstraints {
+			copy.dropConstraints[i] = constraint.clone()
+		}
+	}
+	if stmt.entity != nil {
+		copy.entity = stmt.entity.clone()
+	}
+	if stmt.on != nil {
+		copy.on = stmt.on.clone()
+	}
+	if stmt.renameColumn != nil {
+		copy.renameColumn = &columnRename{
+			column: stmt.renameColumn.column.clone(),
+			name:   stmt.renameColumn.name,
+		}
+	}
+	if stmt.renameConstraint != nil {
+		copy.renameConstraint = &constraintRename{
+			constraint: stmt.renameConstraint.constraint.clone(),
+			name:       stmt.renameConstraint.name,
+		}
+	}
 	return &copy
 }
 func (stmt *stmtAlter) render(baseRenderer *baseRenderer) error {

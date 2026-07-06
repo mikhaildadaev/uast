da3f2fad05dd5907a9f145eb7950b55c51e59fc7
mikhaildadaev <mikhaildadaev@gmail.com>
2026-07-06 13:32:31 +0300

    Обновление ядра

--- core_source.go
+++ core_source.go
@@ -213,7 +213,7 @@ func (source *sourceColumn[T]) render(baseRenderer *baseRenderer) error {
 	baseRenderer.renderName(source.getName())
 	baseRenderer.renderOperator(uastCompositeSingleSpace)
 	baseRenderer.renderService(baseRenderer.config.listTypes[source.valueType])
-	for _, attr := range baseRenderer.config.orderSupportAttr {
+	for _, attr := range baseRenderer.config.orderSupportCreate {
 		switch attr {
 		case uastModifierAutoIncrement:
 			if source.isAutoIncrement {
